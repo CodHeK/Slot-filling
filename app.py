bee192c45aaac616488ad7e58d20f05dbc2ad9b6
CodHeK <gaganganapathyas@yahoo.in>
2020-02-11 10:46:51 +0800

    readme

--- app.py
+++ app.py
@@ -31,8 +31,13 @@ def sendResponse(responseObj):
     response.headers.add('Access-Control-Allow-Credentials', True)
     return response
 
-
-@app.route('/api/v1/slots/', methods=['GET'])
+'''
+Ex:
+    http://localhost:9009/api/v1/slots/?sentence=
+    Where%20is%20the%20stop%20for%20USAir%20flight%20number%2037%20from%20
+    Philadelphia%20to%20San%20Francisco%20flying%20next%20friday
+'''
+@app.route('/api/v1/slots', methods=['GET'])
 def main():
     sentence = str(request.args.get('sentence'))
     with graph.as_default():
@@ -44,6 +49,7 @@ def main():
         'response_time': str(response_time)[:4] + 's'
     }
     return sendResponse(response)
+
 
 if __name__ == '__main__':
     '''
